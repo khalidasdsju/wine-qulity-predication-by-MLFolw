6f360f624bfad632bf60365c054df87fabcb5f93
Mir Khalid Hassan <khalid.asds.ju@gmalil .com>
2025-04-09 12:14:02 +0600

    add data_ingestion

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from mlProject import logger
-logger.info("Logging has been set up.")
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_ingestion import DataIngestion
+from mlProject.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SCHEMA_FILE_PATH
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    config = ConfigurationManager(
+        config_filepath=CONFIG_FILE_PATH,
+        params_filepath=PARAMS_FILE_PATH,
+        schema_filepath=SCHEMA_FILE_PATH
+    )
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+##  data_ingestion.extract_zip_file()
+except Exception as e:
+    raise e
--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,130 +1,113 @@
 import os
-from box.exceptions import BoxValueError
-import yaml
-from mlProject import logger
 import json
 import joblib
-from ensure import ensure_annotations
-from box import ConfigBox
+import yaml
 from pathlib import Path
-from typing import Any
-
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from typing import Any, List
+from mlProject import logger
+from ensure import ensure_annotations
 
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns
-
-    Args:
-        path_to_yaml (str): path like input
-
-    Raises:
-        ValueError: if yaml file is empty
-        e: empty file
-
-    Returns:
-        ConfigBox: ConfigBox type
-    """
+    """Reads a YAML file and returns its content as a ConfigBox object."""
     try:
-        with open(path_to_yaml) as yaml_file:
+        with open(path_to_yaml, "r") as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            if content is None:
+                raise ValueError(f"YAML file {path_to_yaml} is empty")
+            logger.info(f"YAML file loaded successfully from: {path_to_yaml}")
             return ConfigBox(content)
-    except BoxValueError:
-        raise ValueError("yaml file is empty")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"YAML file {path_to_yaml} not found.")
+    except yaml.YAMLError as e:
+        raise ValueError(f"Error while parsing YAML file {path_to_yaml}: {e}")
     except Exception as e:
         raise e
-    
 
 
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories
+    """Creates a list of directories if they do not exist.
 
     Args:
-        path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+        path_to_directories (list): List of paths to directories to create.
+        verbose (bool, optional): If True, logs the directory creation. Defaults to True.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at: {path}")
+            logger.info(f"Created directory at: {path}")
 
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
-    """save json data
+    """Saves a dictionary as a JSON file.
 
     Args:
-        path (Path): path to json file
-        data (dict): data to be saved in json file
+        path (Path): Path to the JSON file.
+        data (dict): Data to be saved in the JSON file.
     """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
-
-    logger.info(f"json file saved at: {path}")
-
-
+    logger.info(f"JSON file saved at: {path}")
 
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
-    """load json files data
+    """Loads data from a JSON file and returns it as a ConfigBox.
 
     Args:
-        path (Path): path to json file
+        path (Path): Path to the JSON file.
 
     Returns:
-        ConfigBox: data as class attributes instead of dict
+        ConfigBox: Data as class attributes instead of a dictionary.
     """
     with open(path) as f:
         content = json.load(f)
-
-    logger.info(f"json file loaded succesfully from: {path}")
+    logger.info(f"JSON file loaded successfully from: {path}")
     return ConfigBox(content)
 
 
 @ensure_annotations
 def save_bin(data: Any, path: Path):
-    """save binary file
+    """Saves data as a binary file using joblib.
 
     Args:
-        data (Any): data to be saved as binary
-        path (Path): path to binary file
+        data (Any): Data to be saved as a binary file.
+        path (Path): Path to the binary file.
     """
     joblib.dump(value=data, filename=path)
-    logger.info(f"binary file saved at: {path}")
+    logger.info(f"Binary file saved at: {path}")
 
 
 @ensure_annotations
 def load_bin(path: Path) -> Any:
-    """load binary data
+    """Loads data from a binary file.
 
     Args:
-        path (Path): path to binary file
+        path (Path): Path to the binary file.
 
     Returns:
-        Any: object stored in the file
+        Any: Object stored in the binary file.
     """
     data = joblib.load(path)
-    logger.info(f"binary file loaded from: {path}")
+    logger.info(f"Binary file loaded from: {path}")
     return data
-
 
 
 @ensure_annotations
 def get_size(path: Path) -> str:
-    """get size in KB
+    """Gets the size of a file in KB.
 
     Args:
-        path (Path): path of the file
+        path (Path): Path to the file.
 
     Returns:
-        str: size in KB
+        str: Size in KB.
     """
-    size_in_kb = round(os.path.getsize(path)/1024)
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    logger.info(f"Size of the file {path} is {size_in_kb} KB")
     return f"~ {size_in_kb} KB"
-
-
-
-
